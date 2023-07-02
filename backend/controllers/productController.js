67ecc27dbb00e01cc6db141e9adc4ee712d4a0ad
rajesh <cisr6146@gmail.com>
2023-07-02 17:02:22 +0530

    deploy

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -42,9 +42,14 @@ exports.getProducts = catchAsyncError(async (req, res, next) => {
 exports.newProduct = catchAsyncError(async (req, res, next) => {
   let images = [];
 
+  let BASE_URL = process.env.BACKEND_URL;
+  if (process.env.NODE_ENV === "production") {
+    BASE_URL = `${req.protocol}://${req.get("host")}`;
+  }
+
   if (req.files.length > 0) {
     req.files.forEach((file) => {
-      let url = `${process.env.BACKEND_URL}/uploads/product/${file.originalname}`;
+      let url = `${BASE_URL}/uploads/product/${file.originalname}`;
       images.push({ image: url });
     });
   }
@@ -88,10 +93,15 @@ exports.updateProduct = catchAsyncError(async (req, res, next) => {
     images = product.images;
   }
 
+  let BASE_URL = process.env.BACKEND_URL;
+  if (process.env.NODE_ENV === "production") {
+    BASE_URL = `${req.protocol}://${req.get("host")}`;
+  }
+
   console.log(req.files);
   if (req.files.length > 0) {
     req.files.forEach((file) => {
-      let url = `${process.env.BACKEND_URL}/uploads/product/${file.originalname}`;
+      let url = `${BASE_URL}/uploads/product/${file.originalname}`;
       images.push({ image: url });
     });
   }
